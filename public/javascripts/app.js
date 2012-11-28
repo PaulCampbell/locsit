3df031bb886a96391a4bdb24b5b34abc92902e95
paulcampbell <paulgcampbell@gmail.com>
2012-11-28 20:38:06 +0000

    heatmap overlay on the page

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,4 +1,4 @@
-var App = (function () {
+var App = (function (google, HeatmapOverlay) {
 	var Api = {},
      map,
      markers,
@@ -7,14 +7,22 @@ var App = (function () {
      mapOptions,
      google,
      mapCentreLat,
-     mapCentreLong ;
+     mapCentreLong,
+     tweets,
+     heatmapMarkers,
+    heatmap;
+
+    var testData={
+        		max: 10,
+        		data: [{lat: 53.567719, lng:-1.044, count: 4},{lat: 51.5608, lng:-1.724, count: 7}]
+    }
 
 
-	function init(google) {
+	function init() {
         markers = [];
         infoWindows = [];
-        mapCentreLat = '53.567719';
-        mapCentreLong = '-1.483154';
+        mapCentreLat = 53.567719;
+        mapCentreLong = -1.483154;
         mapOptions = {
             center: new google.maps.LatLng(mapCentreLat, mapCentreLong),
             zoom: 6,
@@ -23,15 +31,30 @@ var App = (function () {
             scaleControl: true,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         };
+        heatmapMarkers = [];
+        tweets = [];
+
         map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+        heatmap = new HeatmapOverlay(map, {"radius":15, "visible":true, "opacity":60});
+
+         google.maps.event.addListenerOnce(map, "idle", function(){
+            heatmap.setDataSet(testData);
+        });
 	}
+
+
 	
-	Api.Init = function(google){
-        init(google);
+	Api.Init = function(){
+        init();
     };
 
+    Api.UpdateMap = function()
+    {
+
+    }
+
 	return Api;
-}());
+}(google, HeatmapOverlay));
 
 
-App.Init(window.google);
+App.Init();
